ea4843dbfbabe98466a73ef7d4af5f0c13256b26
AdminWhaleFall <whalefall9420@outlook.com>
2022-05-09 00:58:03 +0800

    🦄 refactor: 优化项目结构，优化日志显示。

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+default_header = {
+    "User-Agent": "Mozilla/5.0 (Linux; U; Android 10; zh-cn; Mi 10 Build/QKQ1.191117.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/79.0.3945.147 Mobile Safari/537.36 XiaoMi/MiuiBrowser/13.5.40"
+}
